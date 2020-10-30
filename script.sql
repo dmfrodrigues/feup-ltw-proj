2d9e42003a53c46e4b499e1dd2a9eecc534bcfcf
2dukes <ruipinto02@hotmail.com>
2020-10-30 16:39:58 +0000

    Created sqlite script.

---
+++ script.sql
@@ -0,0 +1,135 @@
+.mode column
+.headers ON
+
+PRAGMA foreign_keys=ON;
+PRAGMA encoding="UTF-8";
+
+CREATE TABLE Shelter (
+    id INTEGER,
+    name VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    pictureUrl VARCHAR,
+    registeredOn INTEGER NOT NULL,
+
+    CONSTRAINT Shelter_PK PRIMARY KEY(id)
+);
+
+CREATE TABLE User (
+    id INTEGER,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    pictureUrl VARCHAR,
+    registeredOn INTEGER NOT NULL,
+    shelter INTEGER,
+
+    CONSTRAINT User_PK PRIMARY KEY(id)
+    CONSTRAINT User_FK FOREIGN KEY (shelter) REFERENCES Shelter ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE Admin (
+    id INTEGER,
+
+    CONSTRAINT Admin_PK PRIMARY KEY(id),
+    CONSTRAINT Admin_FK FOREIGN KEY(id) REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Notification (
+    id INTEGER,
+    read INTEGER DEFAULT 0 NOT NULL,
+    subject VARCHAR NOT NULL,
+    text VARCHAR,
+    userId INTEGER NOT NULL,
+
+    CONSTRAINT Notification_PK PRIMARY KEY(id),
+    CONSTRAINT Notification_FK FOREIGN KEY(userId) REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Pet (
+    id INTEGER,
+    name VARCHAR,
+    species VARCHAR NOT NULL,
+    size VARCHAR NOT NULL,
+    color VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    status VARCHAR DEFAULT 'forAdoption' NOT NULL,
+    postedBy INTEGER NOT NULL,
+
+    CONSTRAINT Pet_PK PRIMARY KEY(id),
+    CONSTRAINT Pet_FK FOREIGN KEY(postedBy) REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT sizeRule CHECK (size LIKE 'XS' OR
+                               size LIKE 'S' OR
+                               size LIKE 'M' OR
+                               size LIKE 'L' OR
+                               size LIKE 'XL'),
+
+    CONSTRAINT statusRule CHECK (status LIKE 'forAdoption' OR
+                                 status LIKE 'addopted' OR
+                                 status LIKE 'delivered')
+);
+
+CREATE TABLE PetPhoto (
+    petId INTEGER,
+    url VARCHAR NOT NULL, -- Assuming every row has an URL
+
+    CONSTRAINT PetPhoto_PK PRIMARY KEY(petId),
+    CONSTRAINT PetPhoto_FK FOREIGN KEY(petId) REFERENCES Pet ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE AdoptionRequest (
+    id INTEGER NOT NULL,
+    text VARCHAR NOT NULL,
+    outcome VARCHAR DEFAULT 'pending' NOT NULL,
+    pet INTEGER NOT NULL,
+    user INTEGER NOT NULL, -- Response User
+
+    CONSTRAINT AdoptionRequest_PK PRIMARY KEY(id),
+    CONSTRAINT AdoptionRequest_FK1 FOREIGN KEY(pet) REFERENCES Pet ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT AdoptionRequest_FK2 FOREIGN KEY(user) REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT outcomeRule CHECK (outcome LIKE 'pending' OR
+                                  outcome LIKE 'accepted' OR
+                                  outcome LIKE 'rejected')
+);
+
+CREATE TABLE AdoptionRequestMessage (
+    id INTEGER,
+    text VARCHAR NOT NULL,
+    request INTEGER NOT NULL,
+
+    CONSTRAINT AdoptionRequestMessage_PK PRIMARY KEY(id),
+    CONSTRAINT AdoptionRequestMessage_FK FOREIGN KEY(request) REFERENCES AdoptionRequest ON DELETE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Comment (
+    id INTEGER,
+    postedOn INTEGER NOT NULL,
+    text VARCHAR NOT NULL,
+    pet INTEGER NOT NULL,
+    user INTEGER NOT NULL,
+
+    CONSTRAINT Comment_PK PRIMARY KEY(id),
+    CONSTRAINT Comment_FK1 FOREIGN KEY(user) REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT Comment_FK2 FOREIGN KEY(pet) REFERENCES Pet ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE Table AnswerTo (
+    idFather INTEGER, -- Referal
+    idSon INTEGER NOT NULL,
+
+    CONSTRAINT AnswerTo_PK PRIMARY KEY(idSon),
+    CONSTRAINT AnswerTo_FK1 FOREIGN KEY(idFather) REFERENCES Comment ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT AnswerTo_FK2 FOREIGN KEY(idSon) REFERENCES Comment ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE PetPhotoInComment (
+    commentId INTEGER
+    url VARCHAR NOT NULL, -- Assuming every row has an URL
+
+    CONSTRAINT PetPhotoInComment_PK PRIMARY KEY(commentId),
+    CONSTRAINT PetPhotoInComment_FK FOREIGN KEY(commentId) REFERENCES Comment ON DELETE CASCADE ON UPDATE CASCADE
+);
