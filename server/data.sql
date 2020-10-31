eadbd5d985af35031025c90b625fd7fc842cd5f8
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-31 14:28:17 +0000

    Added missing passwords

--- server/data.sql
+++ server/data.sql
@@ -4,9 +4,9 @@ INSERT INTO User(username, password, name, pictureUrl) VALUES
 ("BrenoAccioly", "6ee278a3b837d564187ccb3ecc877057701eb7b2", "Breno Accioly", "https://avatars3.githubusercontent.com/u/43919387?s=400&u=dd552fba69b0469db1c0bb3911e13fd46733bd4d&v=4"),
 ("2dukes", "463e890d73f5d6d1f3b1268ee7e426933370f8cb", "Rui Pinto", "https://avatars0.githubusercontent.com/u/22317258?s=400&u=392e493cb6abe197adf4c572550abafcf9db7f27&v=4"),
 ("TiagooGomess", "0d47223986b750aecf2607ee8f26c58f7fe31e3a", "Tiago Gomes", "https://avatars3.githubusercontent.com/u/50210213?s=400&u=037938f7505165a289343a0fb9c7cfac66b29cfd&v=4"),
-("asa", , "Arnaldo dos Santos Almeida", "https://picsum.photos/seed/picsum/400/400"),
-("balves", , "Bernardo Alves", "https://picsum.photos/seed/picsum/400/400"),
-("amz", , "Ana Maria Zevedo", "https://picsum.photos/seed/picsum/400/400")
+("asa", "65417b70a1a7bd08a6189f4d309d90979cbe7b56", "Arnaldo dos Santos Almeida", "https://picsum.photos/seed/picsum/400/400"),
+("balves", "c00ab69a0a4826bb8f8fb5bdf68503f92183a9be", "Bernardo Alves", "https://picsum.photos/seed/picsum/400/400"),
+("amz", "013564dc05c22e397a30d38805fae295ef8cb4cc", "Ana Maria Zevedo", "https://picsum.photos/seed/picsum/400/400")
 ;
 
 INSERT INTO Admin VALUES ("dmfr"), ("BrenoAccioly"), ("2dukes"), ("TiagooGomess");
