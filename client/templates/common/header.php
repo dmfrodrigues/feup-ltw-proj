3f36753fe0aa467d493618de0d8586d2f1ab48ce
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-31 13:53:37 +0000

    Added header, footer and index

---
+++ client/templates/common/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Forever home</title>    
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header>
+            <h1><a href="index.php">Forever home</a></h1>
+        </header>
