f32813dde7a001fd2853e2388f7469361320c7f8
BrenoAccioly <breno.accioly91@gmail.com>
2020-10-31 17:16:06 +0000

    Added login files

---
+++ client/templates/authentication/login.php
@@ -0,0 +1,19 @@
+ <section id="login">
+    
+    <header><h2>Sign in</h2></header>
+
+    <form method="post" action="../../actions/action_login.php">
+      <label> User Name:<br>
+      <input type="text" name="username" placeholder="username" required></label>
+      <br>
+      <label> Password:<br>
+      <input type="password" name="password" placeholder="password" required></label>
+      <br>
+      <input type="submit" value="SIGN IN">
+    </form>
+    <a href="">Forgot Password?</a>
+    <footer>
+      <p>New User?<br><a href="signup.php">SIGN UP</a></p>
+    </footer>
+
+</section>
