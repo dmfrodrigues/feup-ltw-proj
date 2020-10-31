0ff79bc254131630d8d43492eff6ece4ea1e0a05
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-31 16:55:05 +0000

    Minor change to list_pets

--- client/templates/pets/list_pets.php
+++ client/templates/pets/list_pets.php
@@ -1,10 +1,11 @@
+<h1>Pets available for adoption</h1>
 <?php
 foreach ($pets as $pet) {
     $intro = explode(PHP_EOL, $pet['description'])[0];
 ?>
     <article class="pet">
         <header>
-            <h1><a href="pet.php?id=<?= $pet['id'] ?>"><?= $pet['name'] ?></a></h1>
+            <h2><a href="pet.php?id=<?= $pet['id'] ?>"><?= $pet['name'] ?></a></h2>
         </header>
         <img src="<?= $pet['photoUrl'] ?>" alt="photo of <?= $pet['name'] ?>" />
         <p><?= $intro ?></p>
