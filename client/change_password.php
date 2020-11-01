bb3dfdfdea981d78bbab02e2db1f9ccdf747e84f
Tiago Gomes <up201806658@fe.up.pt>
2020-11-01 23:48:16 +0000

    Edit profile almost done; still need to implement action_edit_profile and action_edit_password

---
+++ client/change_password.php
@@ -0,0 +1,12 @@
+<article id="change-password">
+    <form action="action_change_password.php?username=<?= $user['username']?>" method="post">
+        <header>
+            <h1>Change Password</h1>
+        </header>
+        <section id="password">
+            <h3>New Password:</h3>
+            <input type="password" name="password">
+        </section>
+        <input type="submit" value="Submit">
+    </form>
+</article>
--- client/templates/users/edit_profile.php
+++ client/templates/users/edit_profile.php
@@ -1,24 +1,25 @@
-<section id="edit-profile">
-    <header>
-        <h1>Edit Profile</h1>
-    </header>
-    <section id="profile-photo">
-        <h3>Profile Photo:</h3>
-        <img src="<?=$user['pictureUrl']?>">
-        <h1>✎</h1></br>
-    </section>
-    <section id="name">
-        <h3>Name:</h3>
-        <img src="<?=$user['name']?>">
-        <h1>✎</h1></br>
-    </section>
-    <section id="username">
-        <h3>Username:</h3>
-        <img src="<?=$user['username']?>">
-        <h1>✎</h1></br>
-    </section>
-    <section id="password">
-        <h3>Change password</h3>
-        <h1>✎</h1></br>
-    </section>
-</section>
+<article id="edit-profile">
+    <form action="action_edit_profile.php?username=<?= $user['username']?>" method="post">
+        <header>
+            <h1>Edit Profile</h1>
+        </header>
+        <section id="profile-photo">
+            <h3>Profile Photo:</h3>
+            <img src="<?=$user['pictureUrl']?>">
+            <h1>✎</h1></br>
+        </section>
+        <section id="name">
+            <h3>Name:</h3>
+            <textarea name="name"><?=$user['name']?></textarea>
+        </section>
+        <section id="username">
+            <h3>Username:</h3>
+            <textarea name="username"><?=$user['username']?></textarea>
+        </section>
+        <section id="password">
+            <h3>Change password</h3>
+            <a href="change_password.php?username=<?= $user['username']?>"><h1>✎</h1></br></a>
+        </section>
+        <input type="submit" value="Submit">
+    </form>
+</article>
