b965fe32d2813a75f6f7132cbca5896bbd706c53
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-01 00:04:56 +0000

    Added favicon

--- client/templates/common/header.php
+++ client/templates/common/header.php
@@ -4,6 +4,7 @@
         <title>Forever home</title>    
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image/ico" href="resources/favicon.ico">
     </head>
     <body>
         <header>
