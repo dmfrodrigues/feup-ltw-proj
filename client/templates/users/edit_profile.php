d46c82d89afcb33ab9a5384af6532bc7bb5eae88
Tiago Gomes <up201806658@fe.up.pt>
2020-11-01 23:10:05 +0000

    Created edit profile page

---
+++ client/templates/users/edit_profile.php
@@ -0,0 +1,24 @@
+<section id="edit-profile">
+    <header>
+        <h1>Edit Profile</h1>
+    </header>
+    <section id="profile-photo">
+        <h3>Profile Photo:</h3>
+        <img src="<?=$user['pictureUrl']?>">
+        <h1>✎</h1></br>
+    </section>
+    <section id="name">
+        <h3>Name:</h3>
+        <img src="<?=$user['name']?>">
+        <h1>✎</h1></br>
+    </section>
+    <section id="username">
+        <h3>Username:</h3>
+        <img src="<?=$user['username']?>">
+        <h1>✎</h1></br>
+    </section>
+    <section id="password">
+        <h3>Change password</h3>
+        <h1>✎</h1></br>
+    </section>
+</section>
