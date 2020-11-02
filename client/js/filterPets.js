2cd01239de6cfacec916f8326e997cc0eaedaf77
Tiago Gomes <up201806658@fe.up.pt>
2020-11-02 19:45:11 +0000

    Simplified the code

--- client/js/filterPets.js
+++ client/js/filterPets.js
@@ -1,36 +1,24 @@
-function filterByName() {
-    let input, filter, pet_name;
-    input = document.getElementById("name");
-    filter = input.value.toUpperCase();
-
-    let visible_elements = Array.from(document.querySelectorAll('article.pet'));
-    
-    visible_elements = visible_elements.filter((pet_element) => {
-        pet_name = pet_element.querySelector('header h2 a').innerHTML;
-        return (pet_name.toUpperCase().indexOf(filter) > -1); 
-    });
-
-    return visible_elements;
-}
 
 function filterByParameter(parameter_id, query_selector, visible_elements) {
-    let input, filter, pet_name;
+    let input, filter, pet_parameter;
     input = document.getElementById(parameter_id);
     filter = input.value.toUpperCase();
 
     visible_elements = visible_elements.filter((pet_element) => {
-        pet_name = pet_element.querySelector(query_selector).innerHTML;
-        return (pet_name.toUpperCase().indexOf(filter) > -1);
+        pet_parameter = pet_element.querySelector(query_selector).innerHTML;
+        return (pet_parameter.toUpperCase().indexOf(filter) > -1);
     });
 
     return visible_elements;
 }
 
 function filterByAllParameters() {
-    
-    let filtered = filterByName();
-    let parameters_ids = ["location", "species", "age", "color", "size", "sex"];
-    let query_selectors = ["p#hidden_location", "p#hidden_species", "p#hidden_age", "p#hidden_color", "p#hidden_size", "p#hidden_sex"];
+
+    const parameters_ids = ["name", "location", "species", "age", "color", "size", "sex"];
+    const query_selectors = ["header h2 a", "p#hidden_location", "p#hidden_species", "p#hidden_age", "p#hidden_color", "p#hidden_size", "p#hidden_sex"];
+
+    let filtered = Array.from(document.querySelectorAll('article.pet'));
+
     for (let i = 0; i < parameters_ids.length; i++) {
         filtered = filterByParameter(parameters_ids[i],query_selectors[i],filtered);
     }
