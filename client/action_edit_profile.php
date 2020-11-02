e439ce17bc4b3913d221f5984b6eab0cf7ceae8b
Tiago Gomes <up201806658@fe.up.pt>
2020-11-02 00:32:26 +0000

    action_edit_profile bug fixed

--- client/action_edit_profile.php
+++ client/action_edit_profile.php
@@ -6,15 +6,17 @@ include_once('../server/users.php');
 $user = getUser($_GET['username']);
 
 if (isset($_SESSION['username'])){
+    echo "Yes";
     if($_SESSION['username'] != $user["username"]){
         header('Location: profile.php?username='.$_GET['username'].'&failed=1');
         die();
     }
 
     editUser(
-        $_POST["name"],
-        $_POST["username"]
+        $_POST["username"],
+        $_POST["name"]
     );
+
     header('Location: profile.php?username='.$_GET['username']);
 }
 
