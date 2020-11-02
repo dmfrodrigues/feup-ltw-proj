302c15c9ee4c0401540503b8ad89870ec7c3d72b
Tiago Gomes <up201806658@fe.up.pt>
2020-11-02 19:12:00 +0000

    Now it's possible to search for a pet by multiple parameters

--- client/js/filterPets.js
+++ client/js/filterPets.js
@@ -29,19 +29,93 @@ function filterByLocation(visible_elements) {
     return visible_elements;
 }
 
+function filterBySpecies(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("species");
+    filter = input.value.toUpperCase();
+
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_species').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1);
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+function filterByAge(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("age");
+    filter = input.value.toUpperCase();
+
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_age').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1);
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+function filterByColor(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("color");
+    filter = input.value.toUpperCase();
+
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_color').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1);
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+function filterBySize(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("size");
+    filter = input.value.toUpperCase();
+
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_size').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1);
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+function filterBySex(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("sex");
+    filter = input.value.toUpperCase();
+
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_sex').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1);
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+
 function filterByAllParameters() {
     
     let filterNames = filterByName();
     let filterLocations = filterByLocation(filterNames);
+    let filterSpecies = filterBySpecies(filterLocations);
+    let filterAge = filterByAge(filterSpecies);
+    let filterColor = filterByColor(filterAge);
+    let filterSize = filterBySize(filterColor);
+    let filterSex = filterBySex(filterSize);
     
     let petList = document.querySelector('section#pet-list').children;
     
     for(let x = 0; x < petList.length; x++) 
         petList[x].style.display = "none";
-    
-    console.log(petList);    
 
-    filterLocations.forEach((article) => {
+    filterSex.forEach((article) => {
         console.log(article);
         document.querySelector('section#pet-list').appendChild(article);
         article.style.display = "";
