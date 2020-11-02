9ec87d9f2c76e5861a8ace073d3b349fc7dd30cd
2dukes <ruipinto02@hotmail.com>
2020-11-02 18:33:34 +0000

    Multiple filters (name, location) applied

--- client/js/filterPets.js
+++ client/js/filterPets.js
@@ -3,17 +3,48 @@ function filterByName() {
     input = document.getElementById("name");
     filter = input.value.toUpperCase();
     
-    let pet_elements = document.querySelectorAll('article.pet');
+    let pet_elements = Array.from(document.querySelectorAll('article.pet'));
+    let visible_elements = pet_elements;
     
-    for (let i = 0; i < pet_elements.length; i++) {
-        pet_name = pet_elements[i].querySelector('header h2 a').innerHTML;
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('header h2 a').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1); 
+    });
 
-        if (pet_name.toUpperCase().indexOf(filter) > -1) {
-            pet_elements[i].style.display = "";
-        } 
-        else {
-            pet_elements[i].style.display = "none";
-        }
-    }
+    console.log(visible_elements);
+    return visible_elements;
 }
 
+function filterByLocation(visible_elements) {
+    let input, filter, pet_name;
+    input = document.getElementById("location");
+    filter = input.value.toUpperCase();
+    
+    visible_elements = visible_elements.filter((pet_element) => {
+        pet_name = pet_element.querySelector('p#hidden_location').innerHTML;
+        return (pet_name.toUpperCase().indexOf(filter) > -1); 
+    });
+
+    console.log(visible_elements);
+    return visible_elements;
+}
+
+function filterByAllParameters() {
+    
+    let filterNames = filterByName();
+    let filterLocations = filterByLocation(filterNames);
+    
+    let petList = document.querySelector('section#pet-list').children;
+    
+    for(let x = 0; x < petList.length; x++) 
+        petList[x].style.display = "none";
+    
+    console.log(petList);    
+
+    filterLocations.forEach((article) => {
+        console.log(article);
+        document.querySelector('section#pet-list').appendChild(article);
+        article.style.display = "";
+    });
+      
+}
--- client/templates/pets/list_pets.php
+++ client/templates/pets/list_pets.php
@@ -1,15 +1,15 @@
-<section id="pet-list">
-    <h1>Pets available for adoption</h1>
-    <script src="js/filterPets.js"></script>
+<h1>Pets available for adoption</h1>
+<script src="js/filterPets.js"></script>
 
-    <article id="searches">
-        <input type="text" id="name" onkeyup="filterByName()" placeholder="Search for names.." title="Pet name">
-        <input type="text" id="location" onkeyup="filterByName()" placeholder="Search for locations.." title="Pet location">
-        <!-- <input type="text" id="name" onkeyup="filterByName()" placeholder="Search for names.." title="Pet name"> -->
-        
+<article id="searches">
+    <input type="text" id="name" onkeyup="filterByAllParameters()" placeholder="Pet name" title="Pet name">
+    <input type="text" id="location" onkeyup="filterByAllParameters()" placeholder="Pet location" title="Pet location">
+    <!-- <input type="text" id="name" onkeyup="filterByName()" placeholder="Search for names.." title="Pet name"> -->
     
-    </article>
+
+</article>
     
+<section id="pet-list">
 
     <?php
     foreach ($pets as $pet) {
@@ -22,30 +22,10 @@
             </header>
             <img src="<?= $photoUrl ?>" alt="photo of <?= $pet['name'] ?>" />
             <p><?= $intro ?></p>
+            <p id="hidden_location" style="display: none"><?= $pet['location'] ?></p>
+            <?php echo $pet['location']; ?>
         </article>
     <?php
     }
     ?>
 </section>
-
-
-<!-- <script>
-function filterByName() {
-    let input, filter, pet_element, pet_names_array, pet_name;
-    input = document.getElementById("name");
-    filter = input.value.toUpperCase();
-    
-    pet_elements = document.querySelectorAll('article.pet');
-    
-    for (let i = 0; i < pet_elements.length; i++) {
-        pet_name = pet_elements[i].querySelector('header h2 a').innerHTML;
-
-        if (pet_name.toUpperCase().indexOf(filter) > -1) {
-            pet_elements[i].style.display = "";
-        } 
-        else {
-            pet_elements[i].style.display = "none";
-        }
-    }
-}
-</script> -->
