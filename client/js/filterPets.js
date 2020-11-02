a7407a29a227a00bbe9b5d559578510d4a123763
Tiago Gomes <up201806658@fe.up.pt>
2020-11-02 21:49:45 +0000

    Added function suggested by @dmfrodrigues

--- client/js/filterPets.js
+++ client/js/filterPets.js
@@ -1,43 +1,23 @@
-
-function filterByParameter(parameter_id, query_selector, visible_elements) {
-    let input, filter, pet_parameter;
-    input = document.getElementById(parameter_id);
-    filter = input.value.toUpperCase();
-
-    visible_elements = visible_elements.filter((pet_element) => {
-        pet_parameter = pet_element.querySelector(query_selector).innerHTML;
-        return (pet_parameter.toUpperCase().indexOf(filter) > -1);
-    });
-
-    return visible_elements;
-}
-
 function filterByAllParameters() {
 
     const parameters_ids = ["name", "location", "species", "age", "color", "size", "sex"];
     const query_selectors = ["header h2 a", "p#hidden_location", "p#hidden_species", "p#hidden_age", "p#hidden_color", "p#hidden_size", "p#hidden_sex"];
 
-    let filtered = Array.from(document.querySelectorAll('article.pet'));
+    let pet_elements = document.querySelectorAll('article.pet');
+    for(let i = 0; i < pet_elements.length; ++i){
+        let pet_element = pet_elements[i];
+        let isVisible = true;
 
-    for (let i = 0; i < parameters_ids.length; i++) {
-        filtered = filterByParameter(parameters_ids[i],query_selectors[i],filtered);
+        for(let j = 0; j < parameters_ids.length; ++j){
+            let parameter_id = parameters_ids[j];
+            let query_selector = query_selectors[j];
+
+            let pet_parameter = pet_element.querySelector(query_selector).innerHTML;
+            isVisible &= (pet_parameter.toUpperCase().indexOf(document.getElementById(parameter_id).value.toUpperCase()) > -1);
+            if(!isVisible) break;
+        }
+
+        pet_element.style.display = (isVisible ? "" : "none");
     }
-    
-    let petList = document.querySelector('section#pet-list').children;
-    
-    // removing the visibility of all the elements
-    for(let x = 0; x < petList.length; x++) 
-        petList[x].style.display = "none";
-
-    // sort elements by pet name
-    filtered.sort((a, b) => {
-        return a.firstElementChild.firstElementChild.innerText.toLowerCase().localeCompare(b.firstElementChild.firstElementChild.innerText.toLowerCase());
-    });
-
-    filtered.forEach((article) => {
-        document.querySelector('section#pet-list').appendChild(article);
-        article.style.display = ""; // making the filtered elements visible
-    });
-
 
 }
