ea30168c5c5b6dfb79af46268f20b833ff4e15a3
Tiago Gomes <up201806658@fe.up.pt>
2020-11-05 00:30:32 +0000

    Removed duplicate code

--- client/templates/users/profile_me.php
+++ client/templates/users/profile_me.php
@@ -21,22 +21,9 @@
         </article>
     <?php } ?>
     </section>
-    <section id="favorites">
-    <h2>Favorites</h2>
     <?php
-    if(empty($favorite_pets)) echo '<p>No favorite pets</p>';
-    else
-    foreach ($favorite_pets as $pet) {
-        $photoUrl = getPetMainPhoto($pet['id']);
-        ?>
-        <article class="pet">
-            <header>
-                <h2><a href="pet.php?id=<?= $pet['id']?>"><?= $pet['name'] ?></a></h2>
-            </header>
-            <img src="<?= $photoUrl ?>" alt="photo of <?= $pet['name'] ?>" />
-        </article>
-    <?php } ?>
-    </section>
+    include_once 'templates/pets/view_pets_in_profile.php';
+    ?>
     <section id="actions">
         <h2>Actions</h2>
         <ul>
