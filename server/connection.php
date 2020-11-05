d508407b60194043968f89b3b72b99d62970bae4
2dukes <ruipinto02@hotmail.com>
2020-11-05 00:07:21 +0000

    Fixed edit profile issue with cascading usernames.

--- server/connection.php
+++ server/connection.php
@@ -3,5 +3,6 @@
 include_once __DIR__.'/server.php';
 
 $db = new PDO('sqlite:'.SERVER_DIR.'/database.db');
+$db->exec( 'PRAGMA foreign_keys = ON;' );
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
