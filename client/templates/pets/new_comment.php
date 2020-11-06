5f656c41d8a1808d90027e6b679d097d78bb5d12
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-06 00:27:35 +0000

    Implemented comment adding on the server side

--- client/templates/pets/new_comment.php
+++ client/templates/pets/new_comment.php
@@ -1,9 +1,11 @@
 <form id="new-comment" class="answer" action="action_add_comment.php" method="post">
+    <input id="comment-petId" name="petId" type="hidden" value="<?= $pet['id'] ?>">
+    <input id="comment-username" name="username" type="hidden" value="<?= $user['username'] ?>">
     <input id="comment-answerTo" name="answerTo" type="hidden">
     <article class="comment">
         <span id="comment-user" class="user"><a href="profile.php?username=#">#</a></span>
         <a id="comment-profile-pic-a" class="profile-pic-a" href="profile.php?username?#"><img class="profile-pic" src="#"></a>
-        <textarea id="comment-text" class="comment-text" name="comment-text" placeholder="Write a comment..." rows="1"
+        <textarea id="comment-text" class="comment-text" name="text" placeholder="Write a comment..." rows="1"
             oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
         <input type="submit" id="comment-submit" value="Submit">
     </article>
