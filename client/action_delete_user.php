d32da1e6ddc389b6396aa1361304ea72b4458647
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-07 01:15:08 +0000

    Added logout after the account is deleted

--- client/action_delete_user.php
+++ client/action_delete_user.php
@@ -5,7 +5,9 @@
 
     if(isset($_SESSION['username']) && $_SESSION['username'] == $_GET['username']) {
         deleteUser($_SESSION['username']);
-        header("Location: index.php");
+        session_destroy();
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        die();
     }
     else 
         header('Location: profile.php?username='.$_GET['username'].'&failed=1');
