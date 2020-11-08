0ce322ef571638d39b7f39451982bead48b23cfd
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-08 18:54:47 +0000

    Changed place of functions

--- client/js/commentsTree.js
+++ client/js/commentsTree.js
@@ -1,7 +1,7 @@
 class CommentTree {
     constructor(comments){
     /// Initialize tree
-    let tree = new Map();
+    this.map = new Map();
     this.root = new Comment(
         null,
         null,
@@ -15,7 +15,7 @@ class CommentTree {
     for(let i = 0; i < comments.length; ++i){
         let c = comments[i];
         let id = parseInt(c.id);
-        tree.set(id, new Comment(
+        this.map.set(id, new Comment(
             parseInt(c.id),
             parseInt(c.pet),
             c.user,
@@ -27,16 +27,16 @@ class CommentTree {
         ));
     }
     /// Build tree
-    for(let [id, c] of tree){
+    for(let [id, c] of this.map){
         // Replace parent ID by reference to parent
-        if(c.parent !== null) c.parent = tree.get(c.parent);
+        if(c.parent !== null) c.parent = this.map.get(c.parent);
         // Add children
         if(c.parent === null) this.root.addChild(c);
         else                  c.parent.addChild(c);
     }
     /// Sort children
     this.root.sortChildren();
-    for(let [id, c] of tree)
+    for(let [id, c] of this.map)
         c.sortChildren();
     }
     addToElement(commentsSection){
@@ -48,10 +48,29 @@ class CommentTree {
 }
 
 $(document).ready(function(){
-    let tree = new CommentTree(comments);
+    var tree = new CommentTree(comments);
     let commentsSection = document.getElementById("comments");
     if(typeof user !== 'undefined'){
         commentsSection.appendChild(tree.root.createAnswerElement(user));
     }
     tree.addToElement(commentsSection);
 });
+
+function clickedCommentReply(comment_el){
+    let id_string = comment_el.id;
+    let id = parseInt(id_string.split("-")[1]);
+
+    let new_comment_el = document.getElementById(`new-comment-${id}`);
+    new_comment_el.style.display = (new_comment_el.style.display === "none" ? "" : "none");
+}
+
+function clickedCommentEdit(comment_el){
+    let id_string = comment_el.id;
+    let id = parseInt(id_string.split("-")[1]);
+
+    let article_el = comment_el.querySelector(".comment");
+    article_el.style.display = "none";
+
+    let edit_comment_el = document.getElementById(`edit-comment-${id}`);
+    edit_comment_el.style.display = "";
+}
