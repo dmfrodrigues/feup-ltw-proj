850b78d6352da9faff234203de16782067778d26
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-08 00:53:56 +0000

    Added dynamics

--- client/js/commentsTree.js
+++ client/js/commentsTree.js
@@ -85,6 +85,35 @@ function createAnswerElement(commentId){
     return answerElement;
 }
 
+function createEditElement(commentId, commentElement){
+    let userElement = commentElement.querySelector('#comment-user');
+    let userImgElement = commentElement.querySelector('#comment-profile-pic-a');
+    let textElement = commentElement.querySelector('#comment-text');
+    let commentImgElement = commentElement.querySelector('#comment-picture');
+
+    let editElement = cloneNodeRecursive(document.querySelector("#templates>#edit-comment"));
+    editElement.id = `edit-comment-${commentId}`;
+
+    let el_commentId = editElement.querySelector('#commentId');
+    el_commentId.value = commentId;
+
+    let el_user = editElement.querySelector("#comment-user");
+    el_user.children[0].href = userElement.children[0].href;
+    el_user.children[0].innerHTML = userElement.children[0].innerHTML;
+
+    let el_userPic = editElement.querySelector("#comment-profile-pic-a");
+    el_userPic.href=userImgElement.href;
+    el_userPic.children[0].src = userImgElement.children[0].src;
+
+    let el_text = editElement.querySelector("#comment-text");
+    el_text.value = textElement.innerHTML;
+
+    let el_img = editElement.querySelector("#comment-picture");
+    el_img.src = commentImgElement.attributes.src.value;
+
+    return editElement;
+}
+
 function addCommentToDocument(parent, comment){
     let commentElement = createCommentElement(comment);
     let detailsElement = commentElement.querySelector("details");
@@ -92,6 +121,20 @@ function addCommentToDocument(parent, comment){
     if(typeof user !== 'undefined'){
         let actions_el = commentElement.querySelector(".actions");
         actions_el.style.display = "";
+
+        let user_el = commentElement.querySelector("#comment-user");
+        let user_name = user_el.children[0].innerHTML;
+        if(user_name === user.username){
+            let edit_el = actions_el.querySelector("#action-edit");
+            edit_el.style.display = "";
+        }
+
+        let editElement = createEditElement(comment.id, commentElement);
+        editElement.style.display = "none";
+        commentElement.insertBefore(
+            editElement,
+            detailsElement
+        );
 
         let answerElement = createAnswerElement(comment.id);
         answerElement.style.display="none";
@@ -118,6 +161,17 @@ function clickedCommentReply(comment_el){
 
     let new_comment_el = document.getElementById(`new-comment-${id}`);
     new_comment_el.style.display = (new_comment_el.style.display === "none" ? "" : "none");
+}
+
+function clickedCommentEdit(comment_el){
+    let id_string = comment_el.id;
+    let id = parseInt(id_string.split("-")[1]);
+
+    let article_el = comment_el.querySelector(".comment");
+    article_el.style.display = "none";
+
+    let edit_comment_el = document.getElementById(`edit-comment-${id}`);
+    edit_comment_el.style.display = "";
 }
 
 $(document).ready(function(){
