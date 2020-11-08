e540299123e81e0468ab978d617ecbff40edc842
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-08 18:56:42 +0000

    Fixed indents and spaces

--- client/js/commentsTree.js
+++ client/js/commentsTree.js
@@ -1,62 +1,62 @@
 class CommentTree {
-    constructor(comments){
-    /// Initialize tree
-    this.map = new Map();
-    this.root = new Comment(
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null
-    );
-    for(let i = 0; i < comments.length; ++i){
-        let c = comments[i];
-        let id = parseInt(c.id);
-        this.map.set(id, new Comment(
-            parseInt(c.id),
-            parseInt(c.pet),
-            c.user,
-            c.userPictureUrl,
-            c.postedOn,
-            c.text,
-            c.commentPictureUrl,
-            (c.answerTo === null ? null : parseInt(c.answerTo))
-        ));
+    constructor(comments) {
+        /// Initialize tree
+        this.map = new Map();
+        this.root = new Comment(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+        for (let i = 0; i < comments.length; ++i) {
+            let c = comments[i];
+            let id = parseInt(c.id);
+            this.map.set(id, new Comment(
+                parseInt(c.id),
+                parseInt(c.pet),
+                c.user,
+                c.userPictureUrl,
+                c.postedOn,
+                c.text,
+                c.commentPictureUrl,
+                (c.answerTo === null ? null : parseInt(c.answerTo))
+            ));
+        }
+        /// Build tree
+        for (let [id, c] of this.map) {
+            // Replace parent ID by reference to parent
+            if (c.parent !== null) c.parent = this.map.get(c.parent);
+            // Add children
+            if (c.parent === null) this.root.addChild(c);
+            else c.parent.addChild(c);
+        }
+        /// Sort children
+        this.root.sortChildren();
+        for (let [id, c] of this.map)
+            c.sortChildren();
     }
-    /// Build tree
-    for(let [id, c] of this.map){
-        // Replace parent ID by reference to parent
-        if(c.parent !== null) c.parent = this.map.get(c.parent);
-        // Add children
-        if(c.parent === null) this.root.addChild(c);
-        else                  c.parent.addChild(c);
-    }
-    /// Sort children
-    this.root.sortChildren();
-    for(let [id, c] of this.map)
-        c.sortChildren();
-    }
-    addToElement(commentsSection){
-        for(let i = 0; i < this.root.children.length; ++i){
+    addToElement(commentsSection) {
+        for (let i = 0; i < this.root.children.length; ++i) {
             let child = this.root.children[i];
             child.addToDocument(commentsSection);
         }
     }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     var tree = new CommentTree(comments);
     let commentsSection = document.getElementById("comments");
-    if(typeof user !== 'undefined'){
+    if (typeof user !== 'undefined') {
         commentsSection.appendChild(tree.root.createAnswerElement(user));
     }
     tree.addToElement(commentsSection);
 });
 
-function clickedCommentReply(comment_el){
+function clickedCommentReply(comment_el) {
     let id_string = comment_el.id;
     let id = parseInt(id_string.split("-")[1]);
 
@@ -64,7 +64,7 @@ function clickedCommentReply(comment_el){
     new_comment_el.style.display = (new_comment_el.style.display === "none" ? "" : "none");
 }
 
-function clickedCommentEdit(comment_el){
+function clickedCommentEdit(comment_el) {
     let id_string = comment_el.id;
     let id = parseInt(id_string.split("-")[1]);
 
