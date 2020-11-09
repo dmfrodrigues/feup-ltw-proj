1c869f6f5a47a3493739a077cf61322bcff7c5d0
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-09 18:12:33 +0000

    Minor changes

--- client/templates/pets/adoption_request_buttons.php
+++ client/templates/pets/adoption_request_buttons.php
@@ -7,7 +7,8 @@
             if (getAdoptionRequestOutcome($_SESSION['username'], $pet['id']) === 'pending') { ?>
                 <div id="remove-proposal"><a href="action_remove_proposal.php?id=<?= $pet['id'] ?>" onclick="return confirm('Do you want to remove the adoption request?')"> <img src="resources/img/remove-proposal.svg" height="30px">Remove Proposal</a></span>
             <?php } else if (getAdoptionRequestOutcome($_SESSION['username'], $pet['id']) === 'rejected') { ?>
-                <div id="rejected-proposal">The proposal was rejected :(</div>
+                <div id="rejected-proposal">The proposal was rejected :( Try make another proposal</div>
+                <div id="propose"><a href="add_proposal.php?id=<?= $pet['id'] ?>"> <img src="resources/img/adopt-me.png" height="100px" width="100px"> </a></span>
             <?php } else if (getAdoptionRequestOutcome($_SESSION['username'], $pet['id']) === 'accepted') { ?>
                 <div id="rejected-proposal">The proposal was accepted! :)</div>
             <?php } 
