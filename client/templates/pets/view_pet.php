cffbebe627a7a85256a1a5d8b480ccbd08c9da88
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-10 13:19:10 +0000

    Indented view_pet.php

--- client/templates/pets/view_pet.php
+++ client/templates/pets/view_pet.php
@@ -8,21 +8,19 @@
 <article id="pet">
     <header>
         <div id="pet-photos">
-        <?php
-        $photos = getPetPhotos($pet['id']); 
-        if(empty($photos)) $photoSelected = "resources/img/no-image.svg";
-        else $photoSelected = $photos[0];
-        ?>
-        <img id="pet-selected-img" src="<?= $photoSelected ?>" alt="selected photo" />
-        <div id=pet-photos-row>
-        <img class="selected" src="<?= $photoSelected ?>" alt="photo 1 of <?= $pet['name'] ?>" 
-            onclick="selectPhoto()" />
-        <?php
-            for($i=1; $i < count($photos); $i++){?>
-                <img src="<?= $photos[$i] ?>" alt="photo <?= $i ?> of <?= $pet['name'] ?>" 
-                onclick="selectPhoto()" />
-        <?php } ?>
-        </div>
+            <?php
+            $photos = getPetPhotos($pet['id']);
+            if (empty($photos)) $photoSelected = "resources/img/no-image.svg";
+            else $photoSelected = $photos[0];
+            ?>
+            <img id="pet-selected-img" src="<?= $photoSelected ?>" alt="selected photo" />
+            <div id=pet-photos-row>
+                <img class="selected" src="<?= $photoSelected ?>" alt="photo 1 of <?= $pet['name'] ?>" onclick="selectPhoto()" />
+                <?php
+                for ($i = 1; $i < count($photos); $i++) { ?>
+                    <img src="<?= $photos[$i] ?>" alt="photo <?= $i ?> of <?= $pet['name'] ?>" onclick="selectPhoto()" />
+                <?php } ?>
+            </div>
         </div>
         <div id="data">
             <h1><?= $pet['name'] ?></h1>
@@ -31,16 +29,16 @@
         </div>
         <div id="actions">
             <?php if(isset($_SESSION['username'])) {
-            $favorite_pets = getFavoritePets($_SESSION['username']);
-            if (in_array($pet, $favorite_pets)) { ?>
-                <div id="favorite"><a href="action_remove_favorite.php?username=<?= $_SESSION['username'] ?>&id=<?= $pet['id'] ?>"><img src="resources/img/anti-heart.svg" height="30px">Remove from favorites</a></div>
-            <?php } else { ?>
-                <div id="favorite"><a href="action_add_favorite.php?username=<?= $_SESSION['username'] ?>&id=<?= $pet['id'] ?>"><img src="resources/img/heart.svg" height="30px">Add to favorites</a></div>
+                $favorite_pets = getFavoritePets($_SESSION['username']);
+                if (in_array($pet, $favorite_pets)) { ?>
+                    <div id="favorite"><a href="action_remove_favorite.php?username=<?= $_SESSION['username'] ?>&id=<?= $pet['id'] ?>"><img src="resources/img/anti-heart.svg" height="30px">Remove from favorites</a></div>
+                <?php } else { ?>
+                    <div id="favorite"><a href="action_add_favorite.php?username=<?= $_SESSION['username'] ?>&id=<?= $pet['id'] ?>"><img src="resources/img/heart.svg" height="30px">Add to favorites</a></div>
+                <?php } ?>
+                <div id="ask"><a href="#comments"><img src="resources/img/question-mark.png" height="42px">Ask question</a></div>
+                <?php include_once 'templates/pets/adoption_request_buttons.php'; ?>
             <?php } ?>
-            <div id="ask"><a href="#comments"><img src="resources/img/question-mark.png" height="42px">Ask question</a></div>
-            <?php include_once 'templates/pets/adoption_request_buttons.php'; ?>
         </div>
-    <?php } ?>
     </header>
     <section id="description">
         <h2>Description</h2>
