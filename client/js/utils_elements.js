131911a95278dc9ab783fdf4bcc9764a05c7b113
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-11-11 14:21:47 +0000

    Changed utils

--- client/js/utils_elements.js
+++ client/js/utils_elements.js
@@ -6,3 +6,11 @@ function cloneNodeRecursive(element){
     }
     return ret;
 }
+
+function updateImgFromFile(img, file){
+    var reader = new FileReader();
+    reader.onloadend = function() {
+        img.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+}
