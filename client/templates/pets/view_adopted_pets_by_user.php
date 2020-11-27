afed5bc83e0b55807459ba420d49afab50ba6960
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-27 10:11:58 +0000

    Added a template to view pets in profile

--- client/templates/pets/view_adopted_pets_by_user.php
+++ client/templates/pets/view_adopted_pets_by_user.php
@@ -1,26 +1,10 @@
 <section id="adopted-pets">
     <h2>Adopted Pets</h2>
     <?php
+    include_once 'view_pet_in_profile.php';
     if(empty($adoptedPets)) echo '<p>No adopted pets</p>';
     else
         foreach ($adoptedPets as $pet) {
-            $photoUrl = getPetMainPhoto($pet['id']);
-            $intro = explode(PHP_EOL, $pet['description'])[0];
-            ?>
-            <article class="pet-card" onclick="location.href = 'pet.php?id=<?= $pet['id'] ?>';">
-                <div class="pet-card-inner">
-                    <div class="pet-card-front">
-                        <img src="<?= $photoUrl ?>">
-                        <div class="pet-card-content-front">
-                            <h2><?= $pet['name'] ?></h2>
-                        </div>
-                    </div>
-                    <div class="pet-card-back">
-                        <div class="pet-card-content-back">
-                            <p><?= $intro ?></p>
-                        </div>
-                    </div>
-                </div>
-            </article>
-        <?php } ?>
+            viewPetInProfile($pet);
+        } ?>
 </section>
