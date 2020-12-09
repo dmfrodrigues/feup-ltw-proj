5363e8b903da458c6df1946b259b060c38693e74
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-09 09:44:25 +0000

    Fixed bug in profile shelter others

--- client/templates/shelters/profile_shelter_others.php
+++ client/templates/shelters/profile_shelter_others.php
@@ -2,7 +2,7 @@
     <header>
         <img class="profile-shelter-pic" id="profile_img" src="<?php echo (is_null($shelter['pictureUrl']) ? "resources/img/no-image.svg": $shelter['pictureUrl']) ?>">
         <span id="name"><?=$shelter['name']?></span>
-        <span id="username"><?=$_SESSION['username']?></span>
+        <span id="username"><?=$shelter['username']?></span>
     </header>
     <?php
     include_once 'templates/pets/view_pets_in_shelter_profile.php';
