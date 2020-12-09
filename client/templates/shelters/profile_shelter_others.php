0cf08f120fa1bdd031a9cc31fba4634dd7227c10
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-09 19:46:22 +0000

    Fixed bug in shelter profile when there is no session

--- client/templates/shelters/profile_shelter_others.php
+++ client/templates/shelters/profile_shelter_others.php
@@ -3,7 +3,7 @@
         <img class="profile-shelter-pic" id="profile_img" src="<?php echo (is_null($shelter['pictureUrl']) ? "resources/img/no-image.svg": $shelter['pictureUrl']) ?>">
         <span id="name"><?=$shelter['name']?></span>
         <span id="username"><?=$shelter['username']?></span>
-        <?php if (!isset($_SESSION['isShelter'])) {
+        <?php if (!isset($_SESSION['isShelter']) && isset($_SESSION['username'])) {
             $userShelter = getUserShelter($_SESSION['username']);
             if ($userShelter === $shelter['username']) { ?>
                 <a href="edit_profile.php?username=<?=$userShelter?>">Edit Shelter Profile</a>
