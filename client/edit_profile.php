1b8c53f1592fc2aecc2e193e2a834ae82d3a7ec7
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-09 11:47:21 +0000

    Collaborators can change information about the shelter (not username and password, for security reasons

--- client/edit_profile.php
+++ client/edit_profile.php
@@ -9,11 +9,23 @@ include_once SERVER_DIR . '/shelters.php';
 include_once('templates/common/header.php');
 
 if(isShelter($_GET['username'])) {
-    if(isset($_SESSION['username']) && isset($_SESSION['isShelter']) && $_SESSION['username'] == $_GET['username']) {
+    if(isset($_SESSION['username'])) {
+
         $shelter = getShelter($_GET['username']);
-        $user = getUser($_GET['username']);
-        include_once('templates/users/edit_profile.php');
-        editProfile(true); // true -> is shelter
+        $user = getUser($_SESSION['username']);
+
+
+        if (isset($_SESSION['isShelter']) && $_SESSION['username'] == $_GET['username']) {
+            include_once('templates/users/edit_profile.php');
+            editProfile(true); // true -> is shelter
+        }
+            
+        $userShelter = getUserShelter($user['username']);
+        if ($userShelter === $shelter['username']) {
+            include_once('templates/users/edit_profile.php');
+            editProfile(true); // true -> is shelter
+        }
+
     }
         
 }
--- client/templates/users/edit_profile.php
+++ client/templates/users/edit_profile.php
@@ -1,7 +1,10 @@
 <?php
 function editProfile($isShelter) { 
     global $user;
-    global $shelter; ?>
+    global $shelter;
+    $editUser = null;
+    ($isShelter) ? $editUser = $shelter : $editUser = $user;
+    ?>
     <article id="edit-profile">
     <header>
         <h1>Edit Profile</h1>
@@ -10,7 +13,7 @@ function editProfile($isShelter) {
         <section id="profile-photo">
             <h3>Profile photo</h3>
             <div id="photo">
-                <img class="profile-pic" src="<?php echo (is_null($user['pictureUrl']) ? "resources/img/no-image.svg": $user['pictureUrl']) ?>">
+                <img class="profile-pic" src="<?php echo (is_null($editUser['pictureUrl']) ? "resources/img/no-image.svg": $editUser['pictureUrl']) ?>">
                 <details>
                     <summary>
                         <div class="dropdown-main" id="edit">✎ Edit</button>
@@ -18,7 +21,7 @@ function editProfile($isShelter) {
                     <div class="dropdown" id="edit-options">
                         <ul>
                             <li>
-                                <form action="<?= PROTOCOL_SERVER_URL ?>/actions/edit_profile_picture.php?username=<?= $user['username'] ?>" enctype="multipart/form-data" method="POST">
+                                <form action="<?= PROTOCOL_SERVER_URL ?>/actions/edit_profile_picture.php?username=<?= $editUser['username'] ?>" enctype="multipart/form-data" method="POST">
                                     <script>
                                         function onFileChange(){
                                             if(document.getElementById('profile_picture').value != ''){
@@ -33,36 +36,40 @@ function editProfile($isShelter) {
                                     </label>
                                 </form>
                             </li>
-                            <li <?php if(is_null($user['pictureUrl'])) echo 'class="disabled"';?>>
-                                <a href="<?= PROTOCOL_SERVER_URL ?>/actions/erase_profile_picture.php?username=<?= $user['username']?>">Erase picture</a>
+                            <li <?php if(is_null($editUser['pictureUrl'])) echo 'class="disabled"';?>>
+                                <a href="<?= PROTOCOL_SERVER_URL ?>/actions/erase_profile_picture.php?username=<?= $editUser['username']?>">Erase picture</a>
                             </li>
                         </ul>
                     </div>
                 </details>
             </div>
         </section>
-        <form action="<?= PROTOCOL_SERVER_URL ?>/actions/edit_profile.php?username=<?=$user['username']?>" method="post">
-            <section id="username">
-                <label for="username">Username<input type="text" name="username" placeholder="user's username" value="<?=$user['username']?>" required pattern="^[a-zA-Z0-9]+$"></label>
-            </section>
+        <form action="<?= PROTOCOL_SERVER_URL ?>/actions/edit_profile.php?username=<?=$editUser['username']?>" method="post">
+            <?php if (isset($_SESSION['isShelter']) && isset($_SESSION['username']) && $_SESSION['username'] == $editUser['username']) { ?>
+                <section id="username">
+                    <label for="username">Username<input type="text" name="username" placeholder="user's username" value="<?=$editUser['username']?>" required pattern="^[a-zA-Z0-9]+$"></label>
+                </section>
+            <?php } ?> 
             <section id="name">
-                <label for="name">Name<input type="text" name="name" placeholder="user's name" value="<?=$user['name']?>" required></label>
+                <label for="name">Name<input type="text" name="name" placeholder="user's name" value="<?=$editUser['name']?>" required></label>
             </section>
             <?php if ($isShelter) { ?>
                 <section id="description">
-                    <label for="description">Description<textarea name="description" placeholder="user's description"><?=$shelter['description']?></textarea></label>
+                    <label for="description">Description<textarea name="description" placeholder="user's description"><?=$editUser['description']?></textarea></label>
                 </section>
                 <section id="location">
-                    <label for="location">Location<input type="text" name="location" placeholder="user's location" value="<?=$shelter['location']?>" required></label>
+                    <label for="location">Location<input type="text" name="location" placeholder="user's location" value="<?=$editUser['location']?>" required></label>
                 </section>
             <?php } ?>
-            <section id="password">
-                <label for="password">Password<a href="change_password.php?username=<?= $user['username']?>"><img src="resources/img/edit.svg"></a></label>
-            </section>
+            <?php if (isset($_SESSION['isShelter']) && $_SESSION['username'] == $editUser['username']) { ?>
+                <section id="password">
+                    <label for="password">Password<a href="change_password.php?username=<?= $editUser['username']?>"><img src="resources/img/edit.svg"></a></label>
+                </section>
+            <?php } ?> 
             <input type="submit" value="Submit" id="edit-profile-submit">
         </form>
         <section id='delete-user'>
-            <a href="<?= PROTOCOL_SERVER_URL ?>/actions/delete_user.php?username=<?= $user['username'] ?>" onclick="return confirm('Do you want to delete this account?')">⚠ Delete Account</a>
+            <a href="<?= PROTOCOL_SERVER_URL ?>/actions/delete_user.php?username=<?= $editUser['username'] ?>" onclick="return confirm('Do you want to delete this account?')">⚠ Delete Account</a>
         </section>
     </section>
 </article>
