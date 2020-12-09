440da39571f453b615d1d8d03dd69bbd65fc2fea
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-09 10:21:06 +0000

    Moved more to template file

--- client/templates/pets/comment.php
+++ client/templates/pets/comment.php
@@ -13,13 +13,13 @@
                 <img id="comment-picture" class="comment-picture" src="">
             </div>
             <div id="comment-actions" class="actions" style="display: none;">
-                <img id="action-edit"  class="icon" src="resources/img/edit.svg" onclick="clickedCommentEdit (this.parentNode.parentNode.parentNode)" style="display: none;" title="Edit comment">
+                <img id="action-edit"  class="icon" src="resources/img/edit.svg" onclick="onClickCommentEdit (this.parentNode.parentNode.parentNode)" style="display: none;" title="Edit comment">
                 <form id="action-delete" action="<?= PROTOCOL_SERVER_URL ?>/actions/delete_comment.php" method="post" style="display: none;" title="Delete comment">
                     <input id="action-delete-id" name="id" type="hidden">
                     <input id="action-delete-submit" type="submit" style="display: none">
                     <img class="icon" src="resources/img/trash.svg" onclick="this.parentNode.querySelector('#action-delete-submit').click()">
                 </form>
-                <img id="action-reply" class="icon" src="resources/img/reply.svg" onclick="clickedCommentReply(this.parentNode.parentNode.parentNode)" title="Reply">
+                <img id="action-reply" class="icon" src="resources/img/reply.svg" onclick="onClickCommentReply(this.parentNode.parentNode.parentNode)" title="Reply">
             </div>
         </article>
         <details id="comment-answers" class="comment-details" open>
@@ -28,6 +28,25 @@
     </div>
 </template>
 <script>
+    function onClickCommentEdit(comment_el) {
+        let id_string = comment_el.id;
+        let id = parseInt(id_string.split("-")[1]);
+
+        let article_el = comment_el.querySelector(".comment");
+        article_el.style.display = "none";
+
+        let edit_comment_el = document.getElementById(`edit-comment-${id}`);
+        edit_comment_el.style.display = "";
+    }
+
+    function onClickCommentReply(comment_el) {
+        let id_string = comment_el.id;
+        let id = parseInt(id_string.split("-")[1]);
+
+        let new_comment_el = document.getElementById(`new-comment-${id}`);
+        new_comment_el.style.display = (new_comment_el.style.display === "none" ? "" : "none");
+    }
+
     if(typeof Template === 'undefined') Template = {};
     Template.comment = function (comment) {
         if(typeof Template.comment.template === 'undefined')
