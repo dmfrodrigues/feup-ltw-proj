d5dd92d03293494b5b1c28db0512d79ffbf9f5d6
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-09 16:31:42 +0000

    Minor changes

--- client/templates/pets/comment.php
+++ client/templates/pets/comment.php
@@ -1,31 +1,31 @@
 <template id="comment">
-    <div class="comment">
-        <article class="comment">
-            <span id="comment-user" class="user">
-                <a href="profile.php?username="></a>
-            </span>
-            <a id="comment-profile-pic-a" class="profile-pic-a" href="profile.php?username=">
-                <img class="profile-pic" src="">
-            </a>
-            <span id="comment-date" class="date">2020-31-12 00:00:00</span>
-            <div id="comment-content">
-                <p id="comment-text" class="comment-text"></p>
-                <img id="comment-picture" class="comment-picture" src="">
-            </div>
-            <div id="comment-actions" class="actions" style="display: none;">
-                <img id="action-edit"  class="icon" src="resources/img/edit.svg" onclick="onClickCommentEdit (this.parentNode.parentNode.parentNode)" style="display: none;" title="Edit comment">
-                <form id="action-delete" action="<?= PROTOCOL_SERVER_URL ?>/actions/delete_comment.php" method="post" style="display: none;" title="Delete comment">
-                    <input id="action-delete-id" name="id" type="hidden">
-                    <input id="action-delete-submit" type="submit" style="display: none">
-                    <img class="icon" src="resources/img/trash.svg" onclick="this.parentNode.querySelector('#action-delete-submit').click()">
-                </form>
-                <img id="action-reply" class="icon" src="resources/img/reply.svg" onclick="onClickCommentReply(this.parentNode.parentNode.parentNode)" title="Reply">
-            </div>
-        </article>
-        <details id="comment-answers" class="comment-details" open>
-            <summary></summary>
-        </details>
-    </div>
+<div class="comment">
+    <article class="comment">
+        <span id="comment-user" class="user">
+            <a href="profile.php?username="></a>
+        </span>
+        <a id="comment-profile-pic-a" class="profile-pic-a" href="profile.php?username=">
+            <img class="profile-pic" src="">
+        </a>
+        <span id="comment-date" class="date">2020-31-12 00:00:00</span>
+        <div id="comment-content">
+            <p id="comment-text" class="comment-text"></p>
+            <img id="comment-picture" class="comment-picture" src="">
+        </div>
+        <div id="comment-actions" class="actions" style="display: none;">
+            <img id="action-edit"  class="icon" src="resources/img/edit.svg" onclick="onClickCommentEdit (this.parentNode.parentNode.parentNode)" style="display: none;" title="Edit comment">
+            <form id="action-delete" action="<?= PROTOCOL_SERVER_URL ?>/actions/delete_comment.php" method="post" style="display: none;" title="Delete comment">
+                <input id="action-delete-id" name="id" type="hidden">
+                <input id="action-delete-submit" type="submit" style="display: none">
+                <img class="icon" src="resources/img/trash.svg" onclick="this.parentNode.querySelector('#action-delete-submit').click()">
+            </form>
+            <img id="action-reply" class="icon" src="resources/img/reply.svg" onclick="onClickCommentReply(this.parentNode.parentNode.parentNode)" title="Reply">
+        </div>
+    </article>
+    <details id="comment-answers" class="comment-details" open>
+        <summary></summary>
+    </details>
+</div>
 </template>
 <script>
     function onClickCommentEdit(comment_el) {
--- client/templates/pets/new_comment.php
+++ client/templates/pets/new_comment.php
@@ -1,23 +1,23 @@
 <template id="newComment">
-    <form class="answer" enctype="multipart/form-data" method="put">
-        <input id="comment-petId" name="petId" type="hidden" value="<?= $pet['id'] ?>">
-        <input id="comment-username" name="username" type="hidden" value="<?= $user['username'] ?>">
-        <input id="comment-answerTo" name="answerTo" type="hidden">
-        <input id="comment-picture-input" name="picture" type="file" style="display:none;" onchange="onChangeCommentPictureInput(this)">
-        <article class="comment">
-            <span id="comment-user" class="user"><a href="profile.php?username=#">#</a></span>
-            <a id="comment-profile-pic-a" class="profile-pic-a" href="profile.php?username?#"><img class="profile-pic" src="#"></a>
-            <div id="comment-content">
-                <textarea id="comment-text" class="comment-text" name="text" placeholder="Write a comment..." rows="1"
-                        oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
-                <img id="comment-picture" class="comment-picture" src="">
-            </div>
-            <div id="comment-actions" class="actions">
-                <img class="icon" src="resources/img/annex.svg" onclick="this.parentNode.parentNode.parentNode.querySelector('#comment-picture-input').click()" title="Add picture">
-                <input type="submit" id="comment-submit" value="Submit">
-            </div>
-        </article>
-    </form>
+<form class="answer" enctype="multipart/form-data" method="put">
+    <input id="comment-petId" name="petId" type="hidden" value="<?= $pet['id'] ?>">
+    <input id="comment-username" name="username" type="hidden" value="<?= $user['username'] ?>">
+    <input id="comment-answerTo" name="answerTo" type="hidden">
+    <input id="comment-picture-input" name="picture" type="file" style="display:none;" onchange="onChangeCommentPictureInput(this)">
+    <article class="comment">
+        <span id="comment-user" class="user"><a href="profile.php?username=#">#</a></span>
+        <a id="comment-profile-pic-a" class="profile-pic-a" href="profile.php?username?#"><img class="profile-pic" src="#"></a>
+        <div id="comment-content">
+            <textarea id="comment-text" class="comment-text" name="text" placeholder="Write a comment..." rows="1"
+                    oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'></textarea>
+            <img id="comment-picture" class="comment-picture" src="">
+        </div>
+        <div id="comment-actions" class="actions">
+            <img class="icon" src="resources/img/annex.svg" onclick="this.parentNode.parentNode.parentNode.querySelector('#comment-picture-input').click()" title="Add picture">
+            <input type="submit" id="comment-submit" value="Submit">
+        </div>
+    </article>
+</form>
 </template>
 <script>
     function newComment_checkTextOrImage(newCommentForm){
