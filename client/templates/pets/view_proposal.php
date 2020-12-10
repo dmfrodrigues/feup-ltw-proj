931aeaa03e6c02d6a8d67338557c6b434f7d650d
2dukes <ruipinto02@hotmail.com>
2020-12-10 11:19:21 +0000

    CSS changes regarding proposals.

--- client/templates/pets/view_proposal.php
+++ client/templates/pets/view_proposal.php
@@ -94,8 +94,7 @@
                 <div id="proposal-message-submit">
                     <textarea></textarea>
                     <button class="dark" onclick="addNewAdoptionRequestMsg()" id="submitAnswer">Submit</button>
-                    <div id="refresh-div"><button id="update" class="image" onclick="onClickedUpdateComments(this)"><img src="resources/img/update.svg"/></button></div>
-
+                    <div id="proposal-messages-refresh"><button id="update" class="image" onclick="onClickedUpdateComments(this)"><img src="resources/img/update.svg"/></button></div>
                 </div>  
             </div>
         </div>
