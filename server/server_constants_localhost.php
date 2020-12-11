81af95b72b262b4e1185bdf5b6b410732e92ad40
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-11 13:45:26 +0000

    Mistakes

--- server/server_constants_localhost.php
+++ server/server_constants_localhost.php
@@ -1,5 +1,4 @@
 <?php
-// define('SERVER_DIR', __DIR__);
-define('SERVER_DIR', '/media/DATA/dmfro/Superior/3S1/LTW/ltw-project-g22/server/');
+define('SERVER_DIR', __DIR__);
 define('SERVER_URL', 'localhost/ltw/ltw-project-g22/server');
 define('CLIENT_URL', 'localhost/ltw/ltw-project-g22/client');
