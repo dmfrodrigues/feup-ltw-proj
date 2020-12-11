d792bb87c3ee19d13fc98cc6027c31a84e49ee23
BrenoAccioly <breno.accioly91@gmail.com>
2020-12-11 22:15:33 +0000

    Added error page

---
+++ client/error.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Error</title>
+        <link rel="stylesheet" href="css/error.css">
+        <link rel="shortcut icon" type="image/ico" href="resources/favicon.ico">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
+    </head>
+    <body> 
+        <div id="error"> 
+            <h1>😿</h1>
+            <h3>This page doesn't exist.</h3>
+            <h3><a href="index.php">Go back home</a></h3>
+        </div>
+    </body>
+</html>
