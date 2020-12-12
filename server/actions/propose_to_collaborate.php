77e44feffa023bc11f3da50bbc14e60191339ade
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-12 17:45:30 +0000

    Notification to the user when a shelter proposes to collaborate

--- server/actions/propose_to_collaborate.php
+++ server/actions/propose_to_collaborate.php
@@ -3,12 +3,16 @@ session_start();
 
 require_once __DIR__ . '/../server.php';
 require_once SERVER_DIR.'/connection.php';
+require_once SERVER_DIR.'/notifications.php';
 require_once SERVER_DIR.'/shelters.php';
 
 $username = $_GET['username'];
 
 if (isset($_SESSION['username']) && isset($_SESSION['isShelter'])){
     addShelterInvitation($_POST['description'], $username, $_SESSION['username']);
+
+    addNotification($username, "shelterInvitation", "The shelter " . $_SESSION['username'] . " invited you to be a collaborator.");
+
     header("Location: " . PROTOCOL_CLIENT_URL . "/profile.php?username=$username");
 }
 
