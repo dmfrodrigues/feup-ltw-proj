6a54a9585aa077516ef73fae4d244d1c4164257e
BrenoAccioly <breno.accioly91@gmail.com>
2020-12-12 16:31:09 +0000

    Fixed shelter picture issue

--- client/templates/shelters/view_shelter_card.php
+++ client/templates/shelters/view_shelter_card.php
@@ -1,6 +1,6 @@
 <?php
 function viewShelterCard($shelter) {
-    $shelter_pic = getUserPicture($shelter); ?>
+    $shelter_pic = User::fromDatabase($shelter)->getPictureUrl(); ?>
     <article class="shelter-card" onclick="location.href = 'profile.php?username=<?= $shelter ?>';">
         <div id="shelter-card-content">
             <img src="<?php echo (is_null($shelter_pic) ? "resources/img/no-image.svg" : $shelter_pic)?>">
