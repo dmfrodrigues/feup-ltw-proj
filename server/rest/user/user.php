69d0ec2472409728d0fb09153460d5d06b3f1be8
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-12 17:08:31 +0000

    Preparing user for auth

--- server/rest/user/user.php
+++ server/rest/user/user.php
@@ -5,6 +5,7 @@ require_once SERVER_DIR . '/users.php';
 $user_id_photo_GET = function($args): void{
     $username = $args[1];
     $user = User::fromDatabase($username);
+
     if($user == null){ http_response_code(404); die(); }
     $ret = $user->getPictureUrl();
     if($ret  == null) $ret = CLIENT_URL . '/resources/img/no-image.svg';
@@ -14,7 +15,8 @@ $user_id_photo_GET = function($args): void{
 
 $user_id_photo_PUT = function($args): void{
     $username = $args[1];
-    
+    $user = User::fromDatabase($username);
+
     $file = fopen('php://input', 'r');
     $tmpFilePath = tempnam(sys_get_temp_dir(), 'NEWPROFILEPHOTO');
     $tmpFile = fopen($tmpFilePath, 'w');
@@ -22,13 +24,14 @@ $user_id_photo_PUT = function($args): void{
         fwrite($tmpFile, $data);
     }
 
-    $user = User::fromDatabase($username);
     $user->setPicture($tmpFilePath);
     print_result($user->getPictureUrl());
 };
 
 $user_id_photo_DELETE = function($args): void{
     $id = $args[1];
+    $user = User::fromDatabase($username);
+
 
     try{
         deleteUserPhoto($id);
