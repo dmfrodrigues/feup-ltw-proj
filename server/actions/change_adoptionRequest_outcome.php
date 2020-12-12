4275e267f8690e73cf2766c58db505ee7caf6c79
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-12 18:39:11 +0000

    Notifications to users and shelter associated with a pet when the pet is adopted

--- server/actions/change_adoptionRequest_outcome.php
+++ server/actions/change_adoptionRequest_outcome.php
@@ -6,6 +6,7 @@ require_once SERVER_DIR.'/connection.php';
 require_once SERVER_DIR.'/notifications.php';
 require_once SERVER_DIR.'/users.php';
 require_once SERVER_DIR.'/pets.php';
+require_once SERVER_DIR.'/shelters.php';
 
 if ($_GET['username'] != $_SESSION['username']) 
     header("Location: " . $_SERVER['HTTP_REFERER']);
@@ -20,21 +21,28 @@ if($_GET['outcome'] === 'accepted') {
 
     $userWhoAdopted = Pet::fromDatabase($_GET['petId'])->getAdoptedBy();
     $userWhoPostedPet = $pet->getPostedBy();
-
     addNotification($userWhoAdopted->getUsername(), "adoptionProposalOutcome", "Your proposal for ". $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was accepted.");
 
     foreach($refusedUsers as $refusedUser) {
         addNotification($refusedUser->getUsername(), "proposedPetAdopted", "The pet you proposed, " . $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was adopted by " . $userWhoAdopted->getUsername() . ".");
     }
-
     addNotification($userWhoAdopted->getUsername(), "adoptionProposalOutcome", "Your proposal for ". $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was accepted.");
 
     $usersWhoFavoritePet = getUsersWhoFavoritePet($_GET['petId']);
-    
     foreach($usersWhoFavoritePet as $userWhoFavoritePet) {
         if ($userWhoFavoritePet['username'] !== $userWhoAdopted->getUsername() && $userWhoFavoritePet['username'] !== $userWhoPostedPet->getUsername())
             addNotification($userWhoFavoritePet['username'], "favoriteAdopted", "Your favorite pet " . $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was adopted by " . $userWhoAdopted->getUsername() . ".");
     }
+
+    $shelter = getPetShelter($_GET['petId']);
+    if ($shelter !== null) {
+        addNotification($shelter, "associatedPetAdopted", "Your associated pet " . $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was adopted by " . $userWhoAdopted->getUsername() . ".");
+        $collaborators = Shelter::fromDatabase($shelter)->getCollaborators();
+        foreach($collaborators as $collaborator) {
+            addNotification($collaborator->getUsername(), "associatedPetAdopted", "Your associated pet " . $pet->getName() . ", posted by " . $userWhoPostedPet->getUsername() . " was adopted by " . $userWhoAdopted->getUsername() . ".");
+        }
+    }
+
 }
 
 if($_GET['outcome'] === 'rejected') {    
@@ -42,7 +50,6 @@ if($_GET['outcome'] === 'rejected') {
     $adoptionRequest = getAdoptionRequest($_GET['requestId']);
     $userWhoProposed = $adoptionRequest['user'];
     $userWhoPostedPet = $adoptionRequest['postedBy'];
-
     addNotification($userWhoProposed, "adoptionProposalOutcome", "Your proposal for ". $pet->getName() . ", posted by " . $userWhoPostedPet . " was refused.");
 }
     
