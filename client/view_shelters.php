06f286fbda9b23f869400886c3e0f55621e519d0
BrenoAccioly <breno.accioly91@gmail.com>
2020-12-12 16:37:48 +0000

    Added title to shelters page

--- client/view_shelters.php
+++ client/view_shelters.php
@@ -7,6 +7,7 @@ include_once __DIR__.'/../server/notifications.php';
 include_once SERVER_DIR.'/shelters.php';
 
 $shelters = getAllShelters();
+$title = "Shelters";
 
 include_once 'templates/common/header.php';
 include_once 'templates/shelters/view_shelters.php';
