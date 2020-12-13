14f2687060e4b8821c72beea0d1b29de23f37a5f
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-13 00:01:49 +0000

    Created  function

---
+++ server/rest/notification/notification.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../api_main.php';
+require_once __DIR__ . '/../authentication.php';
+require_once __DIR__ . '/../authorization.php';
+require_once __DIR__ . '/../read.php';
+require_once __DIR__ . '/../print.php';
+require_once SERVER_DIR . '/notifications.php';
+
+$notification_PUT = function(array $args): void{
+    $auth = Authentication\check();
+    Authorization\checkAndRespond(
+        Authorization\Resource::NOTIFICATION,
+        Authorization\Method  ::WRITE  ,
+        $auth,
+        null
+    );
+
+    $_PUT = getPUT();
+    $id = addNotification(
+        $_PUT['username'],
+        $_PUT['subject'],
+        $_PUT['text']
+    );
+};
+
