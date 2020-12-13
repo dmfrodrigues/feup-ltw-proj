786e9147f2447c1e9d06498d2939fc62e10e966b
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-13 00:07:22 +0000

    I only need PUT function by now

--- server/rest/index.php
+++ server/rest/index.php
@@ -32,7 +32,7 @@ $tree = [
         '[0-9]+'                => new ResourceHandlers($adoptionMsg_GET     , null, null                 , null                    )
     ],
     'notification' => [null     => new ResourceHandlers(null                 , null,                  null,                     null),
-        '[0-9]+'                => new ResourceHandlers($notification_GET    , null, $notification_PUT    , $notification_DELETE    )
+        '[0-9]+'                => new ResourceHandlers(null                 , null, $notification_PUT    , null                    )
     ]
 ];
 
