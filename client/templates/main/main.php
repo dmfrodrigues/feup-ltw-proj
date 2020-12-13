0f3fb8f46c2638f536634edc508c24a8c9d5b15f
BrenoAccioly <breno.accioly91@gmail.com>
2020-12-13 00:33:37 +0000

    Main page changes and css

--- client/templates/main/main.php
+++ client/templates/main/main.php
@@ -1,15 +1,12 @@
 <section id="index-body">
     <header>
-        <h3>Cat Adoption Checklist Thinking about adopting a cat? Learn what to consider to ensure a peaceful and happy household.</h3>
+        <h2>A website where users can list rescue pets for adoption and offer them a forever home .</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet vitae risus eget iaculis.
+            Curabitur leo urna, porta eu malesuada ac, congue at lorem. Vivamus sed nibh sapien. Maecenas at.</p>
     </header>
-    <section>
-        <h3><a href="pets.php">View Pets Listed For Adoption</a></h3>
-        <h3><a href="adopted_pets.php">View Adopted Pets</a></h3>
-    </section>
-    <section>
-        <h1>Pets Available for Adoption</h1>
-        <section id="pet-list">
-        <div class="pet-card-grid">
+    <section class="pets-main-page">
+        <h1><a href="pets.php">Pets Available for Adoption</a></h1>
+        <div>
         <?php
         if(count($petsForAdoption) < 3)
             $selected = array_keys($petsForAdoption);
@@ -41,14 +38,16 @@
         </div>
     </section>
 
-    <section>
-        <h1>Recently adopted pets</h1>
-        <?php
+    <section class="pets-main-page">
+        <h1><a href="adopted_pets.php">Recently Adopted Pets</a></h1>
+        <?php  
         if(count($petsAdopted) == 0)
-            echo '<h3>You can be the first to adopt a pet!</h3>';
-        else
-        for($idx=count($petsAdopted); $idx > (count($petsAdopted) - 3); $idx--) {
-            $pet = $petsAdopted[$idx-1];
+            echo '<h3><a href="pets.php">You can be the first to adopt a pet!</a></h3>';
+        else{ ?>
+        <div>
+        <?php
+        for($idx=count($petsAdopted)-1; $idx > (count($petsAdopted) - 3); $idx--) {
+            $pet = $petsAdopted[$idx];
             $intro = explode(PHP_EOL, $pet->getDescription())[0];
             $photoUrl = getPetMainPhoto($pet);
         ?>
@@ -70,5 +69,9 @@
         <?php
         }
         ?>
+        <?php
+        }
+        ?>
+        </div>
     </section>
 </section>
