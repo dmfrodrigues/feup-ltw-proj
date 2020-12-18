30626d210a77b48b656bc00938cd94b1447f0c03
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-18 15:51:39 +0000

    Changed emailing classes

--- server/Email.php
+++ server/Email.php
@@ -10,18 +10,19 @@ require SERVER_DIR.'/../PHPMailer/src/PHPMailer.php';
 require SERVER_DIR.'/../PHPMailer/src/SMTP.php';
 
 /*
-$emailServer = new EmailServer_PHPMailer();
-$emailServer->setHost    ('smtp.gmail.com');
-$emailServer->setUsername('...');
-$emailServer->setPassword('...');
-$emailServer->setFrom    ('foreverhomeorg@gmail.com', 'Forever Home');
+$emailServer = new EmailServer_PHPMailer_Gmail(
+	'...',
+	'...',
+	'...'
+);
 
 $message = new EmailMessage();
-$message->addReceiver('dmfrodrigues2000@gmail.com', 'Diogo Rodrigues');
+$message->addReceiver('a@b.com', 'Arnaldo');
 $message->setSubject ('Password reset');
 $message->setBody    ("Hello world!\r\n\r\nThis is an email.\r\n\r\nBest regards,\r\nForever Home");
 
-$emailServer->sendMessage($message);
+if(!$emailServer->sendMessage($message)) echo "Failed";
+else echo "Success!";
 */
 
 class EmailMessage {
@@ -29,19 +30,16 @@ class EmailMessage {
 	private array $receivers;
 	private string $subject;
 	private string $body;
-	public function addReceiver(string $address, string $name){ $this->receivers[] = ['address' => $address, 'name' => $name]; }
-	public function setSubject (string $subject ){ $this->subject     = $subject;  }
-	public function setBody    (string $body    ){ $this->body        = $body;     }
+	public function addReceiver(string $address, string $name) : void { $this->receivers[] = ['address' => $address, 'name' => $name]; }
+	public function setSubject (string $subject ) : void { $this->subject     = $subject;  }
+	public function setBody    (string $body    ) : void { $this->body        = $body;     }
 	public function getReceivers() : array  { return $this->receivers; }
 	public function getSubject  () : string { return $this->subject;   }
 	public function getBody     () : string { return $this->body;      }
 }
 
 interface EmailServer {
-	public function setHost    (string       $host    );
-	public function setUsername(string       $username);
-	public function setPassword(string       $password);
-	public function sendMessage(EmailMessage $message );
+	public function sendMessage(EmailMessage $message) : bool;
 }
 
 class EmailServer_PHPMailer implements EmailServer {
@@ -50,19 +48,26 @@ class EmailServer_PHPMailer implements EmailServer {
 		$this->mailer = new PHPMailer(true);
 		$this->mailer->SMTPDebug  = SMTP::DEBUG_SERVER;
 		$this->mailer->isSMTP();
-		$this->mailer->SMTPAuth   = true;
 		$this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-		$this->mailer->Port       = 587;									// TCP port to connect to, use 587 by default or 465 for `PHPMailer::ENCRYPTION_SMTPS`
 	}
-	public function setHost    (string $host    ){ $this->mailer->Host     = $host    ; }
-	public function setUsername(string $username){ $this->mailer->Username = $username; }
-	public function setPassword(string $password){ $this->mailer->Password = $password; }
-	public function setFrom    (string $address, string $name){
+
+	public function setHost(string $host) : void {
+		$this->mailer->Host = $host;
+	}
+	public function setPort(int $port) : void {
+		$this->mailer->Port = $port;
+	}
+	public function authenticate(string $username, string $password) : void {
+		$this->mailer->SMTPAuth = true;
+		$this->mailer->Username = $username;
+		$this->mailer->Password = $password;
+	}
+	public function setFrom    (string $address, string $name) : void {
 		$this->mailer->setFrom($address, $name);
 		$this->mailer->addReplyTo($address, $name);
 	}
 
-	public function sendMessage(EmailMessage $message){
+	public function sendMessage(EmailMessage $message) : bool {
 		$receivers = $message->getReceivers();
 		foreach($receivers as $receiver){
 			$this->mailer->addAddress($receiver['address'], $receiver['name']);
@@ -70,6 +75,42 @@ class EmailServer_PHPMailer implements EmailServer {
 		$this->mailer->isHTML(false);
 		$this->mailer->Subject = $message->getSubject();
 		$this->mailer->Body    = $message->getBody();
-		$this->mailer->send();
+		return $this->mailer->send();
 	}
 }
+
+class EmailServer_PHPMailer_Gmail implements EmailServer {
+	private EmailServer_PHPMailer $server;
+	public function __construct(
+		string $address,
+		string $password,
+		string $name
+	){
+		$this->server = new EmailServer_PHPMailer();
+		$this->server->setHost('smtp.gmail.com');
+		$this->server->setPort(587);
+		$this->server->authenticate($address, $password);
+		$this->server->setFrom($address, $name);
+	}
+	public function sendMessage(EmailMessage $message) : bool {
+		return $this->server->sendMessage($message);
+	}
+}
+
+/*
+class EmailServer_PHPMailer_FEUP implements EmailServer {
+	private EmailServer_PHPMailer $server;
+	public function __construct(
+		string $address,
+		string $name
+	){
+		$this->server = new EmailServer_PHPMailer();
+		$this->server->setHost('smtp.fe.up.pt');
+		$this->server->setPort(25);
+		$this->server->setFrom($address, $name);
+	}
+	public function sendMessage(EmailMessage $message) : bool {
+		return $this->server->sendMessage($message);
+	}
+}
+*/
