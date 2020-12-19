e57e3ad152462f11f8c43e36462d2b4670107ba4
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-19 01:22:01 +0000

    Minor fix in rules

--- server/rest/authorization.php
+++ server/rest/authorization.php
@@ -53,9 +53,9 @@ namespace Authorization {
     // add_rule(Resource::FAVORITE_PET            , Method::EDIT , function(\User $user, $resource) : bool { return $user->isAdmin(); });
     
     // ======================================================== PROFILE ========================================================
-    Rules::add_rule(Resource::PROFILE, Method::READ , function(?\User $user, ?\User $otherUser) : bool { return true               ; }); // Everyone can see
-    Rules::add_rule(Resource::PROFILE, Method::WRITE, function(?\User $user, ?\User $otherUser) : bool { return true               ; }); // Everyone can write
-    Rules::add_rule(Resource::PROFILE, Method::EDIT , function(?\User $user, ?\User $otherUser) : bool { return $user == $otherUser; }); // Edit itself
+    Rules::add_rule(Resource::PROFILE, Method::READ , function(?\User $user, ?\User $otherUser) : bool { return true                ; }); // Everyone can see
+    Rules::add_rule(Resource::PROFILE, Method::WRITE, function(?\User $user, ?\User $otherUser) : bool { return true                ; }); // Everyone can write
+    Rules::add_rule(Resource::PROFILE, Method::EDIT , function(?\User $user, ?\User $otherUser) : bool { return $user === $otherUser; }); // Edit itself
     Rules::add_rule(Resource::PROFILE, Method::EDIT , function(?\User $user, ?\User $otherUser) : bool {                                 // Collaborators can edit shelter
         if (!isShelter($otherUser->getUsername())) return false;
         $shelter = \Shelter::fromDatabase($otherUser->getUsername());
