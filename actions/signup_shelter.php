d2b1f19ecebfc7a9222f1468b7746d1f57de4192
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-12-20 16:49:46 +0000

    Minor change

--- actions/signup_shelter.php
+++ actions/signup_shelter.php
@@ -1,8 +1,5 @@
 <?php
 
-// session_start();
-
-require_once __DIR__ . '/../server.php';
 require_once SERVER_DIR.'/database/connection.php';
 require_once SERVER_DIR . '/authentication.php';
 require_once SERVER_DIR.'/classes/Shelter.php';
